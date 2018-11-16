b71b502e79234cc3f905931c23451cfc638a6aec
vkowalsky <vkowalsky@gmail.com>
2018-11-16 10:55:50 +0300

    dagger 2 added

--- app/src/main/java/com/victor/ko/jeench/JeenchApplication.java
+++ app/src/main/java/com/victor/ko/jeench/JeenchApplication.java
@@ -22,56 +22,27 @@ import android.app.Application;
 //import dagger.android.DispatchingAndroidInjector
 //import dagger.android.HasActivityInjector
 //import timber.log.Timber
+import com.victor.ko.jeench.di.AppInjector;
+
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
 
-public class JeenchApp extends Application implements HasActivityInjector {
+public class JeenchApplication extends Application implements HasActivityInjector {
 
     @Inject
     DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
-    public DispatchingAndroidInjector dispatchingAndroidInjector;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        DaggerAppComponent
-                .builder()
-                .application(this)
-                .build()
-                .inject(this);
+        AppInjector.init(this);
     }
 
     @Override
-    public AndroidInjector<Activity> activityInjector() {
+    public DispatchingAndroidInjector<Activity> activityInjector() {
         return dispatchingAndroidInjector;
     }
 }
-/*
-import android.app.Activity
-import android.app.Application
-import com.android.example.github.di.AppInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import timber.log.Timber
-import javax.inject.Inject
-
-
-class GithubApp : Application(), HasActivityInjector {
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-        AppInjector.init(this)
-    }
-
-    override fun activityInjector() = dispatchingAndroidInjector
-}
-*/
