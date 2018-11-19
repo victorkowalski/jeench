076f7d08fb19d92463535a8ddc209236564ade3b
vkowalsky <vkowalsky@gmail.com>
2018-11-19 11:15:17 +0300

    glide added

--- app/src/main/java/com/victor/ko/jeench/view/adapter/CustomBindingAdapter.java
+++ app/src/main/java/com/victor/ko/jeench/view/adapter/CustomBindingAdapter.java
@@ -3,6 +3,8 @@ package com.victor.ko.jeench.view.adapter;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 import androidx.databinding.BindingAdapter;
 
 public class CustomBindingAdapter {
@@ -10,9 +12,26 @@ public class CustomBindingAdapter {
     public static void showHide(View view, boolean show) {
         view.setVisibility(show ? View.VISIBLE : View.GONE);
     }
+
+    @BindingAdapter("imageFromUrl")
+    public static void bindImageFromUrl(ImageView view, String imageUrl) {
+        if (!imageUrl.isEmpty()) {
+            Glide.with(view.getContext())
+                    .load(imageUrl)
+                    //.transition(DrawableTransitionOptions.withCrossFade())
+                    .into(view);
+        }
+    }
+/*
+    @BindingAdapter("bind:imageUrl")
+    public static void loadImage(ImageView view, String url) {
+        Glide.with(view.getContext()).load(url).into(view);
+    }*/
+
     //test
+/*
     @BindingAdapter("imageUrl")
     public static void bindImage(ImageView imageView, String url) {
         Glide.with(fragment).load(url).into(imageView);
-    }
+    }*/
 }
